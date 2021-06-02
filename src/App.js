a3cd870687a64b218e894e460f7462db660a0038
catha <catharinaczipf@hotmail.com>
2021-06-02 10:55:19 +0200

    lets get it started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+import Data from "./data.json";
 
 const element = <FontAwesomeIcon icon={faCoffee} />;
 
 function App() {
+  // const [posts, setPosts] = useState(Data);
   return (
     <React.Fragment>
       <div>{element}</div>
+      {/* <Gallery posts={posts} /> */}
+      <Gallery Data={Data} />
     </React.Fragment>
   );
 }
