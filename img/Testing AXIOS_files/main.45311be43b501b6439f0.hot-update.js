3a3e7c57d3b12343f819031fadb69fd2ae202cff
catha <catharinaczipf@hotmail.com>
2021-06-15 13:01:07 +0200

    Updates

---
+++ img/Testing AXIOS_files/main.45311be43b501b6439f0.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/components/Country.js":
+/*!***********************************!*\
+  !*** ./src/components/Country.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /home/dci/apps/fwb48/dci/testingAxios/src/components/Country.js: Expected corresponding JSX closing tag for <br>. (11:8)\n\n\u001b[0m \u001b[90m  9 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33mh2\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 10 |\u001b[39m           {name} \u001b[33m<\u001b[39m\u001b[33mbr\u001b[39m\u001b[33m>\u001b[39m ({nativeName})\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 11 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh2\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 12 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33mh3\u001b[39m\u001b[33m>\u001b[39m{capital}\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh3\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 13 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33mimg\u001b[39m className\u001b[33m=\u001b[39m\u001b[32m\"flag\"\u001b[39m src\u001b[33m=\u001b[39m{flag} alt\u001b[33m=\u001b[39m\u001b[32m\"country flag\"\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 14 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n    at Object._raise (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:810:17)\n    at Object.raiseWithData (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:803:17)\n    at Object.raise (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:764:17)\n    at Object.jsxParseElementAt (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:5262:16)\n    at Object.jsxParseElementAt (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:5230:32)\n    at Object.jsxParseElementAt (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:5230:32)\n    at Object.jsxParseElement (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:5288:17)\n    at Object.parseExprAtom (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:5295:19)\n    at Object.parseExprSubscripts (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:10954:23)\n    at Object.parseUpdate (/home/dci/apps/fwb48/dci/testingAxios/node_modules/@babel/parser/lib/index.js:10934:21)");
+
+/***/ })
+
+})
+//# sourceMappingURL=main.45311be43b501b6439f0.hot-update.js.map
